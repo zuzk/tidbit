d01308126a21dc1d56d3630965f6c512e57e3728
zuzk <58273995+zuzk@users.noreply.github.com>
2019-12-29 02:19:16 -0500

    Update favicon.js

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -17,9 +17,9 @@ var favicon_images = [
 image_counter = 0; // To keep track of the current image
 
 setInterval(function() {
-    $("link[rel='icon']").remove();
-    $("link[rel='shortcut icon']").remove();
-    $("head").append('<link rel="icon" href="' + favicon_images[image_counter] + '" type="image/gif">');
+    ("link[rel='icon']").remove();
+    ("link[rel='shortcut icon']").remove();
+    ("head").append('<link rel="icon" href="' + favicon_images[image_counter] + '" type="image/gif">');
     
 	if(image_counter == favicon_images.length -1)
         image_counter = 0;
